ae307b43ee972e98d451387fc5ecf125779a87fe
Holly <holly.d.abrams@gmail.com>
2022-04-25 21:43:30 -0400

    First commit

---
+++ 05_reverse_string/reverse_string.py
@@ -0,0 +1,12 @@
+def reverse_string(phrase):
+    """Reverse string,
+
+        >>> reverse_string('awesome')
+        'emosewa'
+
+        >>> reverse_string('sauce')
+        'ecuas'
+    """
+    return phrase[::-1]
+
+    # [start:stop:step] negative step reverses the string
